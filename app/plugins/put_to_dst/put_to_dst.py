eeff658bfa0408c7a8c658110a5c0b143fd1671e
lixinyu <lixinyu44@huawei.com>
2024-01-31 11:39:04 +0800

    functions: add methon of package whl and upload to pypi
    * add style of package whl in build_platform and upload
      function to pypi
    
    Signed-off-by: lixinyu <lixinyu44@huawei.com>

--- app/plugins/put_to_dst/put_to_dst.py
+++ app/plugins/put_to_dst/put_to_dst.py
@@ -12,6 +12,7 @@ See the Mulan PSL v2 for more details.
 import os
 import shutil
 from argparse import _SubParsersAction
+import subprocess
 
 from app.command import Command
 from app.lib import Remote
@@ -34,6 +35,7 @@ class PutToDst(Command):
             """)
 
     def do_add_parser(self, parser_addr:_SubParsersAction):
+        # dst_type is for put the dst to where
         parser_addr.add_argument('-t', '--dst_type', dest="dst_type")
         parser_addr.add_argument('-dd', '--dst_dir', dest="dst_dir")
         parser_addr.add_argument('-ld', '--local_dir', dest="local_dir")
@@ -43,7 +45,7 @@ class PutToDst(Command):
         parser_addr.add_argument('-u', '--remote_dst_user', dest="remote_dst_user", default=None)
         parser_addr.add_argument('-w', '--remote_dst_pwd', dest="remote_dst_pwd", default=None)
         parser_addr.add_argument('-k', '--remote_dst_sshkey', dest="remote_dst_sshkey", default=None)
-
+        parser_addr.add_argument('-ptoken', '--pypi_token', dest="pypi_token", default=None)
         return parser_addr
 
     def do_run(self, args, unknow):
@@ -53,32 +55,112 @@ class PutToDst(Command):
 
         #remote
         if int(args.dst_type) == 0:
-            if args.remote_dst_ip is None or args.remote_dst_user is None or (args.remote_dst_pwd is None and args.remote_dst_sshkey is None):
-                return ValueError("Missing remote related parameters!")
-
-            if args.sign_file:
-                util.add_sum_to_local_dir(local_dir=local_dir)
-
-            self.remote = Remote(
-                remote_ip=args.remote_dst_ip,
-                remote_user=args.remote_dst_user,
-                remote_pwd=args.remote_dst_pwd,
-                remote_key=args.remote_dst_sshkey
-                )
-            self.remote.put_to_remote(
-                local_dir=local_dir,
-                dst_dir=dst_dir,
-                is_delete_dst=args.delete_original)
+            self._put_dst_to_remote(
+                remote_dst_ip = args.remote_dst_ip,
+                remote_dst_user = args.remote_dst_user,
+                remote_dst_pwd = args.remote_dst_pwd,
+                remote_dst_sshkey = args.remote_dst_sshkey,
+                sign_file = args.sign_file,
+                local_dir = local_dir,
+                dst_dir = dst_dir,
+                delete_original = args.delete_original)
 
         #Shared disk or local folder
         elif int(args.dst_type) == 1:
-            if args.sign_file:
-                util.add_sum_to_local_dir(local_dir=local_dir)
-            if args.delete_original:
-                if os.path.isdir(dst_dir):
-                    shutil.rmtree(dst_dir)
-            os.makedirs(os.path.dirname(dst_dir), exist_ok=True)
-            shutil.copytree(local_dir, dst_dir)
+            self._put_dst_to_local(
+                sign_file=args.sign_file,
+                local_dir=local_dir,
+                dst_dir=dst_dir,
+                delete_original=args.delete_original)
+
+        # pypi
+        elif int(args.dst_type) == 2:
+            self._put_dst_to_pypi(local_dir=local_dir, token=args.pypi_token)
         #error value
         else:
             return ValueError("The type value does not have a corresponding destination type!")
+
+    def _put_dst_to_remote(self,
+                           remote_dst_ip,
+                           remote_dst_user,
+                           remote_dst_pwd,
+                           remote_dst_sshkey,
+                           sign_file,
+                           local_dir,
+                           dst_dir,
+                           delete_original):
+        if remote_dst_ip is None or remote_dst_user is None or (remote_dst_pwd is None and remote_dst_sshkey is None):
+            return ValueError("Missing remote related parameters!")
+
+        if sign_file:
+            util.add_sum_to_local_dir(local_dir=local_dir)
+
+        self.remote = Remote(
+            remote_ip=remote_dst_ip,
+            remote_user=remote_dst_user,
+            remote_pwd=remote_dst_pwd,
+            remote_key=remote_dst_sshkey
+            )
+        self.remote.put_to_remote(
+            local_dir=local_dir,
+            dst_dir=dst_dir,
+            is_delete_dst=delete_original)
+
+
+    def _put_dst_to_local(self,
+                            sign_file,
+                            local_dir,
+                            dst_dir,
+                            delete_original):
+        if sign_file:
+            util.add_sum_to_local_dir(local_dir=local_dir)
+        if delete_original:
+            if os.path.isdir(dst_dir):
+                shutil.rmtree(dst_dir)
+        os.makedirs(os.path.dirname(dst_dir), exist_ok=True)
+        shutil.copytree(local_dir, dst_dir)
+
+    def _put_dst_to_pypi(self,
+                         local_dir,
+                         token):
+        # install twine
+        show_res = subprocess.run(
+            "pip show twine",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+            check=False)
+        if show_res.returncode != 0:
+            print("twine is not installed")
+            install_res = subprocess.run(
+                "pip install twine -i https://pypi.tuna.tsinghua.edu.cn/simple",
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True,
+                check=False)
+            if install_res.returncode != 0:
+                raise ValueError("install flake8 faild")
+            print("twine install successful!!!")
+        # check if exists .pypirc
+        with open(os.path.join(os.environ['HOME'], ".pypirc"), mode="w", encoding="utf-8") as f:
+            content = f"""
+[distutils]
+  index-servers =
+    pypi
+
+[pypi]
+  username = __token__
+  password = {token}
+"""
+            f.write(content)
+        if not os.path.isfile(local_dir):
+            raise ValueError(f"{local_dir} is not exists")
+        show_res = subprocess.run(
+            f"twine upload {local_dir}",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+            check=False)
+        if show_res.returncode != 0:
+            raise ValueError(show_res.stderr)
+        print(show_res.stdout)
